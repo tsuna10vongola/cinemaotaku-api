5ed50c199a82efb9896576eb67813e216bccf359
tsuna10vongola <sheriffvongola@gmail.com>
2024-03-14 15:16:42 -0300

    14/3/2024

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 // const Product = require('./models/episodioModel')
-
+mongoose.set('strictQuery', false);
 const app = express()
 app.use(express.json())
 const port = 3000
